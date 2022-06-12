5f09c2834101896b7f4ad066abc8d221ccd34080
Alexander Marquez <alex18escorpio@gmail.com>
2022-06-11 19:41:08 -0500

    update login

--- src/api/login/login.jsx
+++ src/api/login/login.jsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
-import { Alert } from "react-bootstrap";
 import axios from "axios";
 
-const PostLogin = (data) => {
-  console.log("data");
-  console.log(data);
-  const url = process.env.REACT_APP_BACKEND_ENDPOINT.concat("api/user/login");
-  //const url = "http://localhost:5000/api/user/login";
+const api = axios.create({
+  withCredentials: true,
+});
 
-  return axios.post(url, data);
+export const PostLogin = (data) => {
+  const url = process.env.REACT_APP_BACKEND_ENDPOINT.concat("api/user/login");
+  const config = { headers: { "Content-Type": "application/json" } };
+  return api.post(url, data, config);
 };
 
-export { PostLogin };
+export default PostLogin;
