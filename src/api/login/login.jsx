87aeb0dc688be0099833944636c3b76be0aaa716
Alexander Marquez <alex18escorpio@gmail.com>
2022-06-29 09:06:58 -0500

    prueba

--- src/api/login/login.jsx
+++ src/api/login/login.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const PostLogin = (data) => {
-  const config = { headers: { "Content-Type": "application/json" } };
+  //const config = { headers: { "Content-Type": "application/json" } };
   const url = process.env.REACT_APP_BACKEND_ENDPOINT.concat("api/user/login");
 
-  return axios.post(url, data, config);
+  return axios.post(url, data);
 };
 
 const RegisterLogin = (data) => {
